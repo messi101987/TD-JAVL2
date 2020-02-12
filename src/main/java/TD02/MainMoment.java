8f0713e30d670de73369f1a825672bc4679e7e4f
g55315 <55315@etu.he2b.be>
2020-02-12 11:26:21 +0100

    tdOO/ex1.1: attributs et constructeur

---
+++ src/main/java/TD02/MainMoment.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TD02;
+
+/**
+ *
+ * @author g55315
+ */
+public class MainMoment {
+    public static void main(String[] args) {
+        Moment moment1 = new Moment(11,23,55);
+        Moment moment2 = new Moment(23,11,25);
+        Moment moment3 = new Moment(6,14,21);
+        System.out.println(moment1);
+        
+    }
+}
--- src/main/java/TD02/Moment.java
+++ src/main/java/TD02/Moment.java
@@ -10,5 +10,14 @@ package TD02;
  * @author g55315
  */
 public class Moment {
+    private int hour;
+    private int minute;
+    private int second;
     
+    
+    public Moment(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
 }
