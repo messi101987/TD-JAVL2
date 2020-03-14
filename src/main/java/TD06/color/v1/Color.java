cec8b12749590957d3e61bf6ce50daf15be99c72
55315 <55315@etu.he2b.be>
2020-03-14 08:56:50 +0100

    TD06:debut

---
+++ src/main/java/TD06/color/v1/Color.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TD06.color.v1;
+
+/**
+ *
+ * @author oscartison
+ */
+public enum Color {
+    RED,GREEN,BLUE;
+}
