436e97926ede47409cb85ece7c02154fffd068d9
55315 <55315@etu.he2b.be>
2020-03-14 08:47:57 +0100

    TD05

--- src/main/java/TD05/MagicSquare.java
+++ src/main/java/TD05/MagicSquare.java
@@ -14,6 +14,7 @@ public class MagicSquare {
     public int[][] t; 
    
     public MagicSquare() {
+        this.n = 3;
         t = new int[][]{{4, 9, 2}, {3, 5, 7}, {8, 1, 6}};
     }
 
@@ -21,6 +22,7 @@ public class MagicSquare {
         if (n < 3 || n % 2 == 0) {
             throw new IllegalArgumentException("not possible");
         }
+        this.n = n;
         t = new int[n][n];
         int ligne = n - 1;
         int col = n / 2;
@@ -40,5 +42,78 @@ public class MagicSquare {
             t[ligne][col] = ++i;
         }
 
+    }
+    
+    public int sumLine(int num) {
+        if (num < 0 || num > this.n){
+            throw new IllegalArgumentException("n'est pas une ligne valable !");
+        }
+        int res = 0;
+        for(int i = 0; i < this.n; i++){
+            res += t[num][i];
+        }
+        return res;
+    }
+    
+    public int sumColumn(int num) {
+        if (num < 0 || num > this.n){
+            throw new IllegalArgumentException("n'est pas une ligne valable !");
+        }
+        int res = 0;
+        for (int i = 0; i < this.n; i++) {
+            res += t[i][num];
+        }
+        return res;
+    }
+
+    public int sumDiag(int num) {
+        if (num != 1 && num != 2) {
+            throw new IllegalArgumentException("n'est pas un attribut valable !");
+        }
+        int res = 0;
+        if (num == 1) {
+            for (int i = 0; i < n; i++) {
+                res += t[i][i];
+            }
+        } else {
+            for (int i = n - 1, j = 0; i >= 0 && j < n ; i--, j++) {
+                    res += t[j][i];
+                }
+        }
+        return res;
+    }
+    
+    public boolean isMagicSquare(){
+        int sum = sumLine(0);
+        boolean magic = true;
+        for (int i = 0; i<n;i++){
+            magic = (sumLine(i) == sum) && magic;
+            magic = (sumColumn(i) == sum) && magic;
+        }
+        magic = (sumDiag(1) == sumDiag(2)) && magic;
+        return magic;
+    }
+    
+    public void displayMagicSquare() {
+        System.out.printf("Square order : %d \n", this.n);
+        int lg = 0;
+        int col = 0;
+        for (int i = 0; i < n * n; i++) {
+            System.out.printf("%d   ", t[lg][col]);
+            col++;
+            if (col == t[0].length) {
+                System.out.printf("-> %d \n", sumLine(lg));
+                col = 0;
+                lg++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.printf("____ ");
+        }
+        System.out.println("");
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%d   ", sumColumn(i));
+        }
     }
 }
--- src/main/java/TD05/MainMagicSquare.java
+++ src/main/java/TD05/MainMagicSquare.java
@@ -13,5 +13,10 @@ public class MainMagicSquare {
     public static void main(String[] args) {
         MagicSquare a1 = new MagicSquare(7);
         Tableau2D.afficher(a1.t);
+        System.out.println(a1.sumLine(4));
+        System.out.println(a1.sumColumn(2));
+        System.out.println(a1.sumDiag(2));
+        System.out.println(a1.isMagicSquare());
+        a1.displayMagicSquare();
     }
 }
