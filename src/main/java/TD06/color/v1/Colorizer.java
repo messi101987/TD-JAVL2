80d0680ce79e8ada65e734f1e3298ba1ab10c121
Your Name <you@example.com>
2020-03-16 11:30:52 +0100

    Resolution linkage error

---
+++ src/main/java/TD06/color/v1/Colorizer.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TD06.color.v1;
+
+/**
+ *
+ * @author otison
+ */
+public class Colorizer {
+    
+    static String color (Color color, String text){
+        switch(color){
+            case RED:
+                text = "\033[31m" + text + "\033[31m" ;
+                break;
+            case BLUE:
+                text = "\033[34m" + text + "\033[34m" ;
+                break;
+            case GREEN:
+                text = "\033[32m" + text + "\033[32m" ;
+                break;
+        }
+        return  text;
+    }
+}
